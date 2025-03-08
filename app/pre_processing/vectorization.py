c63dba3cee304e21207a7d316050830c55fd1806
ilyesBoukraa <boukraailyesali@gmail.com>
2025-03-08 18:53:07 +0100

    uploading the PDFs and saving them in pincone

--- app/pre_processing/vectorization.py
+++ app/pre_processing/vectorization.py
@@ -1,17 +1,20 @@
 from typing import List
 import uuid
-from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+# from langchain_community.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
+#  from langchain_community.embeddings.huggingface import HuggingFaceInstructEmbeddings
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+
+
+
 # from langchain.vectorstores import FAISS
 from pinecone import Pinecone
 
 def my_vectorization(text_chunks: List[str], index: Pinecone.Index):
-    embeddings = OpenAIEmbeddings()
-    # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    # vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
-    
+    hf = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large", 
+                                       model_kwargs={"device": "cpu"}) 
     vectors = []
     for text in text_chunks:
-        vector = embeddings.embed_query(text)  
+        vector = hf.embed_query(text)  
         vectors.append((str(uuid.uuid4()), vector, {"text": text}))  # Unique ID, vector, metadata
 
     # Upload to Pinecone
